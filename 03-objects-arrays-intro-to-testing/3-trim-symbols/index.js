4eded900bfa419a9efc1f29c6ad1f58820e67749
Eltach00 <eltacquliyev@gmail.com>
2022-06-21 23:21:28 +0300

    решение задачи trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    if (size === 0) return '';
+    let counter = size;
+    let arr = string.split('')
+   .filter( (item, index, narr) => { 
+    if ( !(narr.indexOf(item) === index) ) {
+        --counter;
+        if (item != narr[index-1]) counter = size;
+        if ( counter <= 0 ) {
+            counter = size;
+            return false;
+        }
+        return true;
+    } 
+    return true;
+   } )
+   .join('');
+   return arr;
 }
